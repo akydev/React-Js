c69673cc6346b2d22938b1da4491197c77f3d5a8
akydev <akydev18@gmail.com>
2024-09-24 09:50:33 +0530

    Changes

--- login-authentication/src/Post.tsx
+++ login-authentication/src/Post.tsx
@@ -1,30 +1,22 @@
-import React,{ useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { IPost } from "./interface/IPost";
 import axios from "axios";
-import PostIteam from "./postItem";
+import PostIteam from "./PostItem";
 
-export default function Post(){
+export default function Post() {
+  const [data, setData] = useState<IPost[]>([]);
 
-    const [data, setData] = useState<IPost[]>([]);
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((y) => {
+      setData(y.data);
+    });
+  }, []);
 
-    useEffect(()=>{
-        axios.get("https://jsonplaceholder.typicode.com/posts")
-        .then(y=>{
-            setData(y.data);
-        })
-    })
-
-    return (
-        <div>
-            {
-                data.map((p:IPost, index: number)=>{
-                    return(
-                        <PostIteam d = {p}>
-                            This is the Javascript.
-                        </PostIteam>
-                    )
-                })
-            }
-        </div>
-    )
-};
+  return (
+    <div>
+      {data.map((p: IPost, index: number) => {
+        return <PostIteam d={p}>This is the Javascript.</PostIteam>;
+      })}
+    </div>
+  );
+}
